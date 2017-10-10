651772cca08f57af3eee74e303ea5e3c7f9f45c6
Michael Pauly <micscopau@gmail.com>
2017-10-10 14:45:49 -0600

    Functioning Java list button listener
    Code for Generating lists multiple ways (XML and java based) In order to get button click listener on specific word (and not entire list item) we had to use clunky Java code.
    
    Lecture 3 folloing : http://web.stanford.edu/class/cs193a/lib/

---
+++ app/src/main/java/paulygon/listexperimentation/DictionaryActivity.java
@@ -0,0 +1,60 @@
+package paulygon.listexperimentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import stanford.androidlib.*;
+
+public class DictionaryActivity extends AppCompatActivity {
+
+    private static final String[] WORDS = {
+            "test", "exam",
+            "blank", "empty"
+    };
+
+    private Map<String, String> dictionary;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary);
+
+        dictionary = new HashMap<>();
+
+        for (int i = 0; i < WORDS.length; i +=2){
+            dictionary.put(WORDS[i], WORDS[i+1]);
+
+        }
+
+        ListView list = (ListView) findViewById(R.id.word_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, //activity
+                android.R.layout.simple_list_item_1, //layout,
+                new ArrayList<String>(dictionary.keySet()) //WORDS //array
+        );
+
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String word = parent.getItemAtPosition(position).toString();
+                String defn = dictionary.get(word);
+
+                Toast.makeText(getApplicationContext(), defn, Toast.LENGTH_SHORT).show(); //toast(defn);
+
+            }
+        });
+
+    }
+}
